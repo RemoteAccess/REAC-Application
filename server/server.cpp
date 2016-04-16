dbf98d16def87c5348ff3bf6a19493f0267bc378
scopeInfinity <gagan1kumar@ymail.com>
2016-04-16 18:01:09 +0530

    Upload + Download

--- server/server.cpp
+++ server/server.cpp
@@ -9,10 +9,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <cctype>
 
 using boost::asio::ip::tcp;
 using namespace std;
-#define REAC_PROMPT "manager@REAC> "
 
 int pno;
 
@@ -59,6 +59,11 @@ public:
 	}
 	void start()
 	{
+		boost::asio::async_write(socket, boost::asio::buffer("[[[[REAC:RemoteAccess]]]]\n"),		
+			boost::bind(&reac_communication::write_handler, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+
 		std::cout<<"New user Connected from "<<(IP=socket.remote_endpoint().address().to_string())<<std::endl;
 		ServerPassword();
 		
@@ -67,7 +72,7 @@ public:
 	
 	void write_to_socket(std::string message)
 	{
-		char line[1024];
+		char line[102400];
 		strcpy(line,message.c_str());
 		
 		//while (std::cin.getline(line, 256 + 1))
@@ -116,7 +121,9 @@ public:
 				std::string myString;  
 
 			// Convert streambuf to std::string  
-				std::istream(&response_) >> myString;
+				char buffer[1024];
+				std::istream(&response_).getline(buffer,1024);
+				myString = string(buffer);
 
 				_executor->execute(myString);
 				std::cout<<"[Message Received!] "<<&response_<<std::endl;
@@ -170,7 +177,7 @@ public:
 				ifstream infile;
 			infile.open ("passwd.txt");
 			std::string pass;
-        	while(!infile.eof()) // To get you all the lines.
+        	if(!infile.eof()) // To get you all the lines.
         	{
 	        	getline(infile,pass); // Saves the line in STRING.
 	        	 // Prints our STRING.
@@ -180,12 +187,11 @@ public:
 			if(pass == myString)
 			{
 				sendWelcomeMessage();
-				write_to_socket(REAC_PROMPT);
+				isCommunicating = true;
+				write_to_socket("\n[[Status : Allowed]]\n");
 
 			//Multit	hreading for Writing to Socket
-				isCommunicating = true;
-				char buff[512];
-		
+				
 
 			async_read(); 
 			//std::string cmd = "nc -l "+std::to_string(pno+1)+" | ./lshell  | tee -a log.txt | nc localhost "+std::to_string(pno+2);
@@ -196,6 +202,10 @@ public:
 			}
 			else
 			{
+					isCommunicating = true;
+
+				write_to_socket("\n[[Status : Invalid]]\n");
+				
 				socket.close();
 				isCommunicating = false;
 				return;
@@ -271,14 +281,14 @@ void killProgram(reac_communication *reac, std::string process) {
 	char cmd[500];
 	strcpy(cmd,("kill `pgrep "+process+"`").c_str());
 	int sys = system(cmd);
-	reac->write_to_socket("Status : "+std::to_string(sys));
+	reac->write_to_socket("\n[[Status : "+std::to_string(sys)+"]]\n");
 		
 }
 void killProgramPID(reac_communication *reac, std::string pid) {
 	char cmd[500];
 	strcpy(cmd,("kill "+pid).c_str());
 	int sys = system(cmd);
-	reac->write_to_socket("Status : "+std::to_string(sys));
+	reac->write_to_socket("\n[[Status : "+std::to_string(sys)+"]]\n");
 		
 }	
 
@@ -308,15 +318,39 @@ void executeShell(reac_communication *reac,std::string relPath, std::string argv
 		char *newenv[]={NULL};
 		execve("/bin/sh",argv,newenv);
 		perror("Error in Execution!");
-		reac->write_to_socket("Error in Execution!\n");
+		reac->write_to_socket("\n[[Error in Execution!]]\n");
 		exit(0);
 
 	} else {
-		reac->write_to_socket("PID : "+std::to_string(pid)+"\n");
+		reac->write_to_socket("\n[[PID : "+std::to_string(pid)+"]]\n");
 				
 	}
 }
 
+string escapeString(string s) {
+	string t="";
+	char needEscape[] = {'"'};
+	for (int i = 0; s[i]!='\0'; ++i)
+	{
+		bool flag= false;
+		if(s[i]=='\n' || !(32<=s[i] && s[i]<=126))
+			continue;
+		
+		for (int j = 0; j < sizeof(needEscape)/sizeof(char); ++j)
+		{
+			if(s[i]==needEscape[j])
+			{
+				flag = true;
+				break;
+			}
+		}
+		
+		if(flag)
+			t+="\\";
+		t+=s[i];
+	}
+	return t;	
+}
 
 void executor::execute(std::string str)
 {
@@ -325,7 +359,7 @@ void executor::execute(std::string str)
 
 	if (str == "START_SHELL") {
 		reac->write_to_socket("Shell : Starting\n");
-		system(("/bin/sh ./executor/server_script "+reac->getIP()).c_str());
+		system(("/bin/sh ./executor/server_script "+reac->getIP()+" &").c_str());
 	} else if (str == "EXIT") {
 		reac->close();
 	} else if (str.substr(0,5) == "KILL_") {
@@ -334,7 +368,90 @@ void executor::execute(std::string str)
 		killProgramPID(reac, str.substr(8));
 	} else if (str.substr(0,6) == "BLOCK_") {
 		executeShell(reac,"Log/killer",str.substr(6));
-	} else
+	} else if (str.substr(0,4) == "LOCK") {
+		reac->write_to_socket("\n[[Status : 0]]\n");
+		system("gnome-screensaver-command -l");
+	} else if (str.substr(0,6) == "UPLOAD") {
+		string rest = str.substr(6);
+		
+		if(rest.find("/")==std::string::npos
+			&& rest.find("/")==std::string::npos
+			&& rest.find("\\")==std::string::npos
+			&& rest.find(":")==std::string::npos
+			&& rest.find("*")==std::string::npos
+			&& rest.find("<")==std::string::npos
+			&& rest.find(">")==std::string::npos
+			&& rest.find("|")==std::string::npos
+			) {
+			rest = escapeString(rest);
+
+				system(("nc -l 8083 > \"SHARED/"+rest+"\" &").c_str());
+			//	cout<<"SHARING : |\n\n"<<"nc -l 8083 > \"SHARED/"<<"\" &"<<"|\n\n"<<endl;
+				reac->write_to_socket("\n[[Status : 0]]\n");
+			}
+			else {
+					reac->write_to_socket("\n[[Status : -1]]\n");
+			
+			}
+	} else if (str.substr(0,4) == "PIDS") {
+		// ps -ef | awk '{ printf $2":"$8":" }' | cut -c9- | awk '{ printf "[["$1"]]" }' > .temp
+		system("ps -ef | grep -v \"^root\" | awk '{ printf $2\"!\"$8\"!\" }' | cut -c9- | awk '{ printf \"[[\"$1\"]]\" }' > .temp");
+		
+			ifstream infile;
+			infile.open (".temp");
+			std::string data;
+        	if(!infile.eof()) // To get you all the lines.
+        	{
+	        	getline(infile,data); // Saves the line in STRING.
+	        	 // Prints our STRING.
+        	}
+        	infile.close();
+        	//cout<<data<<endl;
+			
+        	//"[[123:Hello:2312:Its:9321:asdasdf:54451:asdad:4515:adfad]]
+        	//char data2[10000];
+        	//strcpy(data2,.c_str());
+	string ss = "\n"+data+"\n";//"[[123:Hello:2312:Its:9321:asdasdf:54451:asdad:4515:adfad]]\n";
+	reac->write_to_socket(ss);
+	} else if (str.substr(0,6) == "SHARED") {
+		system("ls SHARED/ | paste -d/ -s > .temp");
+		std::string data;
+        	
+			ifstream infile;
+			infile.open (".temp");
+			if(!infile.eof()) // To get you all the lines.
+        	{
+	        	getline(infile,data); // Saves the line in STRING.
+	        }
+        	infile.close();
+        	
+		string ss = "\n[["+data+"]]\n";//"[[123:Hello:2312:Its:9321:asdasdf:54451:asdad:4515:adfad]]\n";
+	 	cout<<ss;
+       reac->write_to_socket(ss);
+	}  else if (str.substr(0,9) == "DOWNLOAD_") {
+		string rest = str.substr(9);
+			//\ / : * ? " < > |
+		if(1 || rest.find("/")==std::string::npos
+			&& rest.find("\\")==std::string::npos
+			&& rest.find(":")==std::string::npos
+			&& rest.find("*")==std::string::npos
+			&& rest.find("<")==std::string::npos
+			&& rest.find(">")==std::string::npos
+			&& rest.find("|")==std::string::npos
+			) {
+				
+				rest = escapeString(rest);
+				system((string("nc ")+reac->getIP()+" 8083 < \"SHARED/"+rest+"\" &").c_str());
+				cout<<(string("nc ")+reac->getIP()+" 8083 < \"SHARED/"+rest+"\" &").c_str()<<endl;
+				reac->write_to_socket("\n[[Status : 0]]\n");
+		}
+		else {
+			cout<<"Invalid File Name";
+			reac->write_to_socket("\n[[Status : -1]]\n");
+	
+		}
+	}
+	else
 		reac->write_to_socket("Invalid REAC Command!\n");
 	
 
